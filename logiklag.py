85d766c316f42d3810411c3879fcb8408abe01da
BuildTools <unconfigured@null.spigotmc.org>
2019-12-11 14:14:02 +0100

    lave først udkast til logiklag

--- logiklag.py
+++ logiklag.py
@@ -1,3 +1,23 @@
+import datalag as dl
 
-class logiklag:
-    def __init__(self):
+
+class LogikLag:
+    def __init__(self, brikker=4, muligheder=6, maks=10, *solution):
+        self.data = dl.datalag(brikker=4, muligheder=6, maks=10, *solution)
+        self.running = True
+
+
+    def gæt(self, *gæt):
+        if len(gæt) is self.data.getBrikker():
+            if all(0 <= value < self.data.getMuligheder() for value in gæt):
+                solution = self.data.getSolution()
+                for value in range(0, self.data.getMuligheder()):
+                    a = gæt.count(value)
+                    if a > solution.count(value):
+                        a = solution.count(value)
+                b = 0
+                for value in range(0, self.data.getBrikker()-1):
+                    if gæt[value] is solution[value]:
+                        b += 1
+                return a, b
+        return False
