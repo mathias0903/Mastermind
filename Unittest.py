776dd260acdd91df19ce776b481ac5feddd93225
BProK <yusufabbassi@gmail.com>
2020-01-06 13:54:27 +0100

    Update Unittest.py

--- Unittest.py
+++ Unittest.py
@@ -23,24 +23,24 @@ class Testfunctions(ut.testcase):
 
     def test_get(self):
         testdatalag = dl.Datalag(brikker=6, muligheder=8, maks=12, solution=(4,6,1,2,3,7))
-        self.assertEqual(testdatalag.getbrikker(), 6)
-        self.assertEqual(testdatalag.getmuligheder(), 8)
-        self.assertEqual(testdatalag.getmaks(), 12)
-        self.assertEqual(testdatalag.getsolution(), (4,6,1,2,3,7))
+        self.assertEqual(testdatalag.brikker=6)
+        self.assertEqual(testdatalag.muligheder=8)
+        self.assertEqual(testdatalag.maks=12)
+        self.assertEqual(testdatalag.solution=(4,6,1,2,3,7))
 
     def test_set(self):
         testdatalag = dl.Datalag(brikker=6, muligheder=8, maks=12, solution=(4,6,1,2,3,7))
-        testdatalag.setbrikker(2)
-        self.assertEqual(testdatalag.getbrikker(), 2)
-        testdatalag.setmuligheder(69)
-        self.assertEqual(testdatalag.getmuligheder(), 69)
-        testdatalag.setmaks(42)
-        self.assertEqual(testdatalag.getmaks(), 42)
-        testdatalag.setsolution(4,6)
-        self.assertEqual(testdatalag.getsolution(), [4,6])
+        testdatalag.brikker=2
+        self.assertEqual(testdatalag.brikker, 2)
+        testdatalag.muligheder=69
+        self.assertEqual(testdatalag.muligheder=69)
+        testdatalag.maks=42
+        self.assertEqual(testdatalag.maks=42)
+        testdatalag.solution=(4,6)
+        self.assertEqual(testdatalag.solution=(4,6)
         with self.assertRaises(TypeError):
-            testdatalag.setbrikker("Epstein didn't kill himself")
+            testdatalag.brikker="Epstein didn't kill himself"
         with self.assertRaises(TypeError):
-            testdatalag.setmuligheder(4.0)
+            testdatalag.muligheder=4.0
         with self.assertRaises(TypeError):
-            testdatalag.setmaks("EXDEE IM PICKLE WICK JOHN WICK FORTNITE UWU JOHN FORTNITE PICKLE WICK UWU")
+            testdatalag.maks="EXDEE IM PICKLE WICK JOHN WICK FORTNITE UWU JOHN FORTNITE PICKLE WICK UWU"
